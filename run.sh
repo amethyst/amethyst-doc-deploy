24886e2b26595526e571616ef37fa75e7171a7dc
Alve Larsson <larsalvelarsson@gmail.com>
2021-02-17 18:46:59 +0100

    init script

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo ---------------------------------------------
+echo - Starting Amethyst git clone...
+echo ---------------------------------------------
+
+git clone https://github.com/amethyst/amethyst.git
+
+cd amethyst
+
+echo ---------------------------------------------
+echo - Moving to latest branch
+echo ---------------------------------------------
+
+git fetch --tags
+
+latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+git checkout $latestTag
+
+
+echo ---------------------------------------------
+echo - Preping rust...
+echo ---------------------------------------------
+
+cargo install mdbook
+mdbook build book
+
+echo Done...
+# From here Netlify grabs amethyst/book/book and host it...
