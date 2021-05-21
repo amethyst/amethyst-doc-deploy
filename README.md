6ca04178039acd79486f8fc234691da18746e355
alvelarsson <larsalvelarsson@gmail.com>
2021-05-22 01:35:09 +0200

    Patch it up

--- README.md
+++ README.md
@@ -1,16 +1,2 @@
-# amethyst-doc-deploy
-deploys the Amethyst docs to Netlify and get's served on https://book.amethyst.rs/stable/
-
-# Amethyst Book (Latest Stable Release)
-[![Netlify Status](https://api.netlify.com/api/v1/badges/28bc58cc-e53c-4a4a-8b30-777795334dc9/deploy-status)](https://app.netlify.com/sites/amethyst-doc-stable/deploys)
-https://book.amethyst.rs/stable/
-
-# Amethyst API (Latest Stable)
--- TODO
-
-
-# Amethyst Book (Latest from main branch) - Unstable
--- TODO
-
-# Amethyst API (Latest from main branch) - Unstable
--- TODO
+# Temporary Doc site
+we are moving the doc build and location into the main amethyst.rs/doc/ site
--- run.sh
+++ run.sh
@@ -53,38 +53,38 @@ mkdir -p ../public/api/stable
 cp -r target/doc/* ../public/api/stable/
 
 echo Skipping latest API and Doc...
-echo ---------------------------------------------
-echo - Moving to latest main
-echo ---------------------------------------------
-
-git checkout main
-
-echo ---------------------------------------------
-echo - Generating Latest Book...
-echo ---------------------------------------------
-
-mdbook build book
-
-echo ---------------------------------------------
-echo - Generating Latest API...
-echo ---------------------------------------------
-
-cargo doc --release --features="vulkan" --no-deps --workspace
-
-echo ---------------------------------------------
-echo - Moving Latest API and Book to public...
-echo ---------------------------------------------
-
-pwd
-
-# Makes docs.amethyst.rs/book/latest
-echo copying book...
-mkdir -p ../public/book/latest
-cp -r book/book/* ../public/book/latest/
-
-# Makes docs.amethyst.rs/api/latest
-echo copying api...
-mkdir -p ../public/api/latest
-cp -r target/doc/* ../public/api/latest/
-
+# echo ---------------------------------------------
+# echo - Moving to latest main
+# echo ---------------------------------------------
+# 
+# git checkout main
+# 
+# echo ---------------------------------------------
+# echo - Generating Latest Book...
+# echo ---------------------------------------------
+# 
+# mdbook build book
+# 
+# echo ---------------------------------------------
+# echo - Generating Latest API...
+# echo ---------------------------------------------
+# 
+# cargo doc --release --features="vulkan" --no-deps --workspace
+# 
+# echo ---------------------------------------------
+# echo - Moving Latest API and Book to public...
+# echo ---------------------------------------------
+# 
+# pwd
+# 
+# # Makes docs.amethyst.rs/book/latest
+# echo copying book...
+# mkdir -p ../public/book/latest
+# cp -r book/book/* ../public/book/latest/
+# 
+# # Makes docs.amethyst.rs/api/latest
+# echo copying api...
+# mkdir -p ../public/api/latest
+# cp -r target/doc/* ../public/api/latest/
+# 
 echo Done...
